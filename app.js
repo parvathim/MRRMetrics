575e31764c4f8fb7d2fb81474a1601217d0881db
LAPTOP-C9H5271T\Parvathi <parvathi6m@gmail.com>
2021-03-05 14:53:14 +0530

    InitialCode

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+require('dotenv').config();
+
+const ChartMogul = require('chartmogul-node');
+const config = new ChartMogul.Config(process.env.CHARTMOGUL_ACCOUNT_TOKEN, process.env.CHARTMOGUL_SECRET_KEY);
+
+ChartMogul.Metrics.mrr(config, {
+    'start-date': '2021-01-01',
+    'end-date': '2021-04-30',
+}).then(res => {
+    var months = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER'];
+    for (let i = 0; i < res.entries.length; i++) {
+        var month = res.entries[i].date.substr(5, 2);
+        console.log(months[month - 1] + ":  " + res.entries[i].mrr);
+    }
+})
